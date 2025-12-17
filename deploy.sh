98544dcfecf73ed07b20eb51cccf72a8e091b095
LibertyWritesCode <faklib3@gmail.com>
2025-12-17 19:49:16 +0000

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Container name
 CONTAINER_NAME="studybud_app"
-IMAGE_NAME="cloudqween/wtf_repo:studybud_v1"
+IMAGE_NAME="engrlibby/groupc_wtfrepo:studybud_v1"
 
 # Path to persistent SQLite file on EC2
 SQLITE_HOST_PATH="/home/ubuntu/sqlite/db.sqlite3"
